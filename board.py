bfbc5cb841cfea869216fa0777ad876f84b7020d
vlcp197 <vlcp197@gmail.com>
2025-01-08 19:56:13 -0300

    implements initialize board and verify if someone won functions

--- board.py
+++ board.py
@@ -1,3 +1,29 @@
-def start_board():
+# initializes an empty game board 
+def start_board():    
+    return [[" " for _ in range(3)] for _ in range(3)]
+
+# the board parameter needs to be a 3X3 list
+def verify_win(board):
+    for i in range(3):
+        # verify if the board have a horizontal line
+        if board[i][0] == board[i][1] == board[i][2] != " ":
+            return True
+        
+        # verify if the board have a vertical line
+        if board[0][i] == board[1][i] == board[2][i] != " ":
+            return True
+
+    # verify if the board have a diagonal line    
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return True
+    
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return True
+    
+    return False
+    
+
+def verify_draw(board):
     ...
+
 
