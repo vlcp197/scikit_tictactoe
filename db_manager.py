73df3eaa026d164f19898ada05aaf4e85fb1717a
vlcp197 <vlcp197@gmail.com>
2025-01-08 21:08:31 -0300

    Add match-related database functions

--- db_manager.py
+++ db_manager.py
@@ -7,7 +7,7 @@ def get_db_connection() -> sqlite3.Row:
     return conn
 
 # Build players table when the app starts. 
-def create_table():
+def create_players_table():
     conn = get_db_connection()
     conn.execute('''
         CREATE TABLE IF NOT EXISTS players (
@@ -21,6 +21,20 @@ def create_table():
     conn.commit()
     conn.close()
 
+def create_game_results_table():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS game_results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            game_id TEXT,
+            player_id TEXT,
+            machine_move TEXT,
+            result TEXT,
+            moves TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 def create_player(player_id: str, player_name: str):
     conn = get_db_connection()
@@ -33,3 +47,13 @@ def create_player(player_id: str, player_name: str):
         )
     conn.commit()
     conn.close()
+
+def save_match_result(game_id: str, player_id: str, machine_move: str, result, moves: int):
+    conn = get_db_connection()
+    conn.execute('''
+        INSERT INTO game_results (game_id, player_id, machine_move, result, moves) 
+        VALUES (?, ?, ?, ?, ?)
+    ''', (game_id, player_id, machine_move, result, str(moves)))
+    conn.commit()
+    conn.close()
+
