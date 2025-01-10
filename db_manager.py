f0bfda4ef3fa9c74ce57614f44f2fca60f783238
vlcp197 <vlcp197@gmail.com>
2025-01-09 21:05:41 -0300

    Remove functions that would not be used; Add functions to register the match; Add functions to update player stats

--- db_manager.py
+++ db_manager.py
@@ -21,21 +21,6 @@ def create_players_table():
     conn.commit()
     conn.close()
 
-def create_game_results_table():
-    conn = get_db_connection()
-    conn.execute('''
-        CREATE TABLE IF NOT EXISTS game_results (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            game_id TEXT,
-            player_id TEXT,
-            machine_move TEXT,
-            result TEXT,
-            moves TEXT
-        )
-    ''')
-    conn.commit()
-    conn.close()
-
 def create_player(player_id: str, player_name: str):
     conn = get_db_connection()
     conn.execute(
@@ -48,12 +33,76 @@ def create_player(player_id: str, player_name: str):
     conn.commit()
     conn.close()
 
-def save_match_result(game_id: str, player_id: str, machine_move: str, result, moves: int):
+def create_match_table():
     conn = get_db_connection()
     conn.execute('''
-        INSERT INTO game_results (game_id, player_id, machine_move, result, moves) 
-        VALUES (?, ?, ?, ?, ?)
-    ''', (game_id, player_id, machine_move, result, str(moves)))
+        CREATE TABLE IF NOT EXISTS match (
+            game_id	TEXT NOT NULL UNIQUE PRIMARY KEY,
+            player_1	TEXT,
+            player_2	TEXT,
+            board_state	TEXT
+        )
+    ''')
     conn.commit()
     conn.close()
+
+
+def create_match(player1_id,player2_id, game_id, board):
+    
+    board = ' | '.join(','.join(inner_list) for inner_list in board)
+
+    conn = get_db_connection()
+    conn.execute(
+        '''
+        INSERT INTO match (game_id, player_1, player_2, board_state) 
+        VALUES (?, ?, ?, ?)
+        ''', 
+        (game_id, player1_id, player2_id, board)
+        )
+    conn.commit()
+    conn.close()
+
+def update_player_win(player_id):
+    ...
+    conn = get_db_connection()
+    conn.execute(
+        '''
+         UPDATE players 
+         SET games_won = games_won + 1
+         WHERE player_id = ?
+       ''', 
+        player_id)
+    conn.commit()
+    conn.close()
+
+def update_player_lose(player_id):
+    ...
+    conn = get_db_connection()
+    conn.execute(
+        '''
+         UPDATE players 
+         SET games_lost = games_lost + 1
+         WHERE player_id = ?
+       ''', 
+        player_id
+        )
+    conn.commit()
+    conn.close()
+
+def update_player_draw(p1,p2):
+    players = [p1, p2]
+
+    conn = get_db_connection()
+    for player in players:
+        conn.execute(
+            '''
+            UPDATE players 
+            SET games_drawn = games_drawn + 1
+            WHERE player_id = ?
+        ''', 
+            player)
+    conn.commit()
+    conn.close()
+
+
 
