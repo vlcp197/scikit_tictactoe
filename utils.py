b66eb587ba79c6d3f8e56d8748b7863e06a341e8
vlcp197 <vlcp197@gmail.com>
2025-01-12 14:59:51 -0300

    refactor and type hinting

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
-def load_dataset(path):
-    columns = ['top-left', 'top-middle', 'top-right', 'middle-left', 'middle-middle', 'middle-right', 'bottom-left', 'bottom-middle', 'bottom-right', 'outcome']
-    data = pd.read_csv(path, header=None, names=columns)
+columns = ['top-left', 'top-middle', 'top-right', 'middle-left', 'middle-middle', 'middle-right', 'bottom-left', 'bottom-middle', 'bottom-right', 'outcome']
+def load_dataset(path: str) -> pd.DataFrame:
+    data=pd.read_csv(path, names=columns)
     if data.empty:
         # Initialize with empty dataset: 9 features (board) + 1 target (best move)
         data = pd.DataFrame(columns=columns)
     return data
 
-def update_dataset(board, outcome, file_path="tic_tac_toe_updated.csv"):
+def update_dataset(board: list[int], outcome:str, file_path="dataset.data"):
     """Update the dataset with the new game outcome."""
-    columns = ['top-left', 'top-middle', 'top-right', 'middle-left', 'middle-middle', 'middle-right', 'bottom-left', 'bottom-middle', 'bottom-right', 'outcome']
     new_data = pd.DataFrame([board + [outcome]], columns=columns)
+    new_data = new_data.replace(0, "b").replace(-1, "o").replace(1, "x")
     new_data.to_csv(file_path, mode='a', header=False, index=False)
     print("Game data saved.")
+
+def position_to_index(row: int, col: int) -> int:
+    """Translate (row, col) position into a number from 0 to 8."""
+    return 3 * row + col
