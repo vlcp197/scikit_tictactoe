89f638cdbea8a2a6d8dafd82c79208f203544633
vlcp197 <vlcp197@gmail.com>
2025-01-12 15:01:45 -0300

    Change the outcome value so the model persues the -1 instead of 1 outcome

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,17 @@
+from numpy import array
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-def preprocessing(data):
+def preprocessing(data: pd.Dataframe) -> list[array]:
     # Encode categorical data (x = 1, o = -1, b = 0)
     mapping = {'x': 1, 'o': -1, 'b': 0}
     for col in data.columns[:-1]:
         data[col] = data[col].map(mapping)
 
-    # Encode outcome (positive = 1, negative = 0)
-    data['outcome'] = data['outcome'].map({'positive': 1, 'negative': 0})
-
+    data['outcome'] = data['outcome'].map({'positive': -1, 'negative': 0})
+    
     # Split data into features and labels
-    X = data[data.columns[:-1]]
+    X = data[list(data.columns[:-1])]
     y = data['outcome']
 
     # Split into training and testing sets
